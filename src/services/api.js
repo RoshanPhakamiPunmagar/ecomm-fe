4098f2a43bc037500a6de547da83360047338ced
Roshan Magar <rajuphakami2020@gmail.com>
2026-03-19 01:07:02 +1100

    code

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,18 @@
+// src/api/api.js
 import axios from "axios";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 const API_BASE_PATH = process.env.REACT_APP_API_BASE_PATH;
 const API_BASE_URL = `${SERVER_URL}${API_BASE_PATH}`;
 
-console.log("API Base URL:", API_BASE_URL); // must show: http://localhost:3000/api/customer/v1
+console.log("API Base URL:", API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true, // allow sending Authorization headers or cookies
 });
 
+// Attach JWT token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessJWT");
   if (token) config.headers.Authorization = `Bearer ${token}`;
